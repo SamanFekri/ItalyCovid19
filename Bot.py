7a470cf92bc9f999db47b3a2489b32a6be160645
skings <samanf74@gmail.com>
2020-04-20 22:11:42 +0200

    fix a problem

--- Bot.py
+++ Bot.py
@@ -127,7 +127,7 @@ def core():
 
 while True:
     x = datetime.today()
-    y = x.replace(day=x.day, hour=x.hour, minute=x.minute + 1, second=0, microsecond=0)
+    y = x.replace(day=x.day + 1, hour=18, minute=30, second=0, microsecond=0)
     delta_t = y - x
 
     secs = delta_t.seconds + 1
